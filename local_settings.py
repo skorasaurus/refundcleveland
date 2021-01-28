eb75e715aa6628c0ebbed21f84175dc0bb908e18
willskora <skorasaurus@gmail.com>
2021-01-28 12:47:23 -0500

    tentative proposal for model

--- local_settings.py
+++ local_settings.py
@@ -1,3 +1,14 @@
 # rename to local_settings.py
 DEBUG="TRUE"
 SECRET_KEY="ENTER KEY HERE" # Django secret key
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/home/skors/python/refundcleveland/db.sqlite3',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
